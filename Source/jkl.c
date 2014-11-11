d561ecbac3f697345d57a5cad4b5c822f2862a87
shy <shy@4bpa.com>
2014-11-12 07:07:31 +0800

    Update jkl.c

--- Source/jkl.c
+++ Source/jkl.c
@@ -3,11 +3,9 @@
 int main(int argc, char* argv[])
 {
  	FILE *fp = fopen("Address.txt", "w");
- 	system("pause");
  	fprintf(fp,"%s",argv[1]);
  	fclose(fp);
  	system("jklbz.exe");
  	getchar();
- 	system("pause");
  	return 0;
 }
